ca9c92ccdc651396fa0135b654625f32ae648602
marvin_lasserre <marvin.lasserre@gmail.com>
2019-05-01 17:51:41 +0200

    Ajout d'un sprite

--- snake.cpp
+++ snake.cpp
@@ -7,7 +7,12 @@ int main()
     sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial",
                             sf::Style::Close | sf::Style::Resize);
     sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
-    player.setFillColor(sf::Color::Green);
+    player.setPosition(206.0f, 206.0f);
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("sprites/idle_1.png");
+    player.setTexture(&playerTexture);
+    //player.setOrigin(50.0f, 50.0f);
+    //player.setFillColor(sf::Color::Green);
 
     while (window.isOpen()){
         sf::Event evt;
@@ -21,15 +26,37 @@ int main()
                               << " New window height: " <<  evt.size.height
                               << std::endl;
                     break;
-                case sf::Event::TextEntered:
-                    if (evt.text.unicode < 128){
-                        printf("%c\n", evt.text.unicode);
-                    }
-                    break;
+                //case sf::Event::TextEntered:
+                    //if (evt.text.unicode < 128){
+                        //printf("%c\n", evt.text.unicode);
+                    //}
+                    //break;
                 default:
                     break;
             }
         }
+
+        /*
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z)){
+            player.move(0.0f, -0.5f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q)){
+            player.move(-0.5f, 0.0f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+            player.move(0.5f, 0.0f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+            player.move(0.0f, 0.5f);
+        }
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+            player.setPosition(static_cast<float>(mousePos.x),
+                               static_cast<float>(mousePos.y));
+        }
+        */
+
+        window.clear();
         window.draw(player);
         window.display();
     }
